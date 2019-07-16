2629910180b0f56f42f7dd16c10ee24c8102cc7d
Ivan <wonderboomhk@gmail.com>
2019-07-16 16:52:41 +0800

    updates

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+
+$('body').awesomeCursor('fas fa-circle');
+
 //To change navigation bar style
 
   $(window).scroll(function(){
@@ -13,21 +16,48 @@ $(document).ready(function(){
     }
   });
 
-//To set active section
-
-//   $(".nav-item").on("click", function(){
-//     $(".navbar-nav").find(".active").removeClass("active");
-//     $(this).addClass("active");
-//   });
-//
-// });
-
-$('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
-  $(".navbar-nav").find(".active").removeClass("active");
-      $(this).addClass("active");
-})
-
+});
 
 //Smooth scrolling effect
 
 var scroll = new SmoothScroll('a[href*="#"]');
+
+
+//Music Sine Wave Animation
+
+(function() {
+
+
+  var run = Pts.quickStart("#pt", "#ffffff"); //background color: white
+
+  run( (time, ftime) => {
+
+    // create a line and get 200 interpolated points
+    let offset = space.size.$multiply(0.2).y;
+    let line = new Group( new Pt( 0, offset ), new Pt( space.size.x, space.size.y-offset ) );
+    let pts = Line.subpoints( line, 200 );
+
+    // get perpendicular unit vectors from each points on the line
+    let pps = pts.map( (p) => Geom.perpendicular( p.$subtract( line[0] ).unit() ).add(p) );
+
+    let angle = space.pointer.x/space.size.x * Const.two_pi * 2;
+
+    // draw each perpendicular like a sine-wave
+    pps.forEach( (pp, i) => {
+      let t = i/200 * Const.two_pi + angle + Num.cycle(time%10000/10000);
+
+      if (i%2===0) {
+        pp[0].to( Geom.interpolate( pts[i], pp[0], Math.sin( t )*offset*2 ) );
+        pp[1].to( pts[i] );
+        form.stroke("#0c6", 2).line(pp);
+      } else {
+        pp[0].to( pts[i] );
+        pp[1].to( Geom.interpolate( pts[i], pp[1], Math.cos( t )*offset*2 ) );
+        form.stroke("#f03", 2).line(pp);
+      }
+
+    });
+
+  });
+
+})();
